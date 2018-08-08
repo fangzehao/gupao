61aab28946823fee4ab1f0d84f0618abd926e724
5being <ljz1318078262@163.com>
2018-08-08 11:16:06 +0800

    提交

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 /**
  * Created by op.101775 on 2018/8/3.
  */
-public class HelloWorld {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
